fb536205e5d594ff545e2f7de9d4c0be594f803a
Arvin Pourabdollah <Arvinpourabdollah@gmail.com>
2023-08-22 18:06:10 +1000

    complete

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
-// Write your code here
+const num1 = 31
+const num2 = 2
+const  multiply = num1* num2;
+
+const  random = parseInt((Math.random()*10))+1
+
+const num3 = 100
+const num4 = 96
+const mod = num3 % num4;
+
+const max = Math.max(10,12,14,16,18,20);
